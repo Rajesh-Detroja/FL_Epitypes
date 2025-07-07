afa82aa554db6d5fe2e72dc54b4ba70abbcd57fe
Rajesh Detroja <rajesh.detroja.93@gmail.com>
2025-07-07 17:05:41 -0400

    Create ClusterAIC.R

---
+++ ClusterAIC.R
@@ -0,0 +1,384 @@
+# R Packages
+# ==========
+library(dplyr)
+library(ggplot2)
+library(reshape)
+library(ggpubr)
+library(factoextra)
+library(readxl)
+library(FactoMineR)
+library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+library(IlluminaHumanMethylation450kmanifest)
+library(stringr)
+library(tidyr)
+library(RColorBrewer)
+library(cluster)
+library(pheatmap)
+library(GGally)
+library(ggfortify)
+
+# Change directory
+setwd("/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/04_Summary/450k/modified_InfiniumPurify/Enmix_quantile_with_QCinfo_Harman_All/05_ClusterAIC/")
+
+# get the 450k annotation data
+ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+ann450k <- as.data.frame(ann450k)
+head(ann450k)
+
+# Define CpGs of interest
+CpG_islands <- ann450k %>% filter(Relation_to_Island == "Island") %>% data.frame() %>% .$Name
+OpenSea <- ann450k %>% filter(Relation_to_Island == "OpenSea") %>% data.frame() %>% .$Name
+S_Shelf <- ann450k %>% filter(Relation_to_Island == "S_Shelf") %>% data.frame() %>% .$Name
+S_Shore <- ann450k %>% filter(Relation_to_Island == "S_Shore") %>% data.frame() %>% .$Name
+N_Shelf <- ann450k %>% filter(Relation_to_Island == "N_Shelf") %>% data.frame() %>% .$Name
+N_Shore <- ann450k %>% filter(Relation_to_Island == "N_Shore") %>% data.frame() %>% .$Name
+
+# Read in methylation data
+bval <- readRDS("/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/01_InfiniumPurify/450k/modified_InfiniumPurify/Enmix_quantile_with_QCinfo_Harman_All/beta_combat_purified.rds")
+mval <- readRDS("/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/01_InfiniumPurify/450k/modified_InfiniumPurify/Enmix_quantile_with_QCinfo_Harman_All/mval_combat_purified.rds")
+
+dim(bval)
+dim(mval)
+
+# Read in sample annotations
+Sample_Annotations <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/db/ANN/20231031_sample_annotations.tsv", sep = "\t", header = TRUE, na.strings = c("","NA")) %>%
+  subset(TYPE!="TFL") %>%
+  subset(TIME_POINT!="T2") %>%
+  filter(SAMPLE_ID %in% colnames(bval))
+
+# Read in sample sheet with batch information
+targets <- read.csv(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/db/SampleSheet/SampleSheet.csv", skip = 1, header = TRUE, na.strings = c("","NA"))
+targets$ID <- gsub(" ", "", paste(targets$Sample_Name,".",targets$Sentrix_Position))
+
+# Remove repeated samples to obtain correct batch information
+rep_rm_samples <- c("LY_FL_535_T1.R08C01", "LY_FL_498_T1.R06C01", "LY_FL_158_T1.R01C01",
+                    "LY_FL_479_T1.R02C01", "LY_FL_488_T1.R06C01", "LY_FL_523_T1.R01C01",
+                    "LY_FL_524_T1.R02C01", "LY_FL_525_T1.R03C01", "LY_FL_527_T1.R01C01",
+                    "LY_FL_529_T1.R01C01", "LY_FL_159_T1.R08C01", "LY_FL_536_T1.R01C01",
+                    "HCT116_DKO_methylated.R08C01", "HCT116_DKO_methylated.R04C01")
+
+targets <- targets[!targets$ID %in% rep_rm_samples,]
+targets$Sample_Name <- str_replace(targets$Sample_Name, "LY_FL_159_T1_rep", "LY_FL_159_T1")
+targets <- targets %>% dplyr::select(Sample_Name, Batch)
+colnames(targets)[1]  <- "SAMPLE_ID"
+targets <- targets %>% filter(SAMPLE_ID %in% colnames(bval))
+
+# Read in the Benign_LN sample annotation
+Benign_LN_sample_ann <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/Benign_LN/SampleSheet/Benign_LN_sample_annotations.tsv",
+                                   sep = "\t",
+                                   header = TRUE,
+                                   na.strings=c("","NA")) %>%
+  dplyr::select(SAMPLE_ID, TYPE)
+
+Benign_LN_sample_ann <- Benign_LN_sample_ann %>% filter(SAMPLE_ID %in% colnames(bval))
+Benign_LN_sample_ann$Batch <- "Benign_LN"
+
+# Read in the NIH sample annotation
+NIH_sample_ann <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/NIH/GSE237299/SampleSheet/GSE237299_sample_annotations.txt",
+                             sep = "\t",
+                             header = TRUE,
+                             na.strings=c("","NA")) %>%
+  dplyr::select(SAMPLE_ID, TYPE)
+
+NIH_sample_ann$SAMPLE_ID <- gsub('-', '_', NIH_sample_ann$SAMPLE_ID)
+NIH_sample_ann <- NIH_sample_ann %>% filter(SAMPLE_ID %in% colnames(bval))
+NIH_sample_ann$Batch <- "GSE237299"
+
+# Read in the EGA sample annotation
+EGA_sample_ann <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/EGA/SampleSheet/Sample_Annotations_EGAD00010001974.txt",
+                             sep = "\t",
+                             header = TRUE,
+                             na.strings=c("","NA")) %>%
+  dplyr::select(SAMPLE_ID, TYPE)
+
+EGA_sample_ann$SAMPLE_ID <- gsub('-', '_', EGA_sample_ann$SAMPLE_ID)
+EGA_sample_ann$SAMPLE_ID <- gsub('\\.', '_', EGA_sample_ann$SAMPLE_ID)
+EGA_sample_ann$Batch <- "EGAD00010001974"
+EGA_sample_ann <- EGA_sample_ann %>% filter(SAMPLE_ID %in% colnames(bval))
+
+# Read in blueprint sample annotation
+Blueprint_sample_ann <- read.csv(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/db/blueprint/Sample_Annotation_EGAS00001001196.txt",
+                                 sep = "\t",
+                                 header = TRUE,
+                                 na.strings=c("","NA")) %>%
+  dplyr::select(Sample_Name, Source_Name, CELL_TYPE, TISSUE_TYPE, DONOR_SEX)
+
+colnames(Blueprint_sample_ann) <- c("SAMPLE_ID", "LY_FL_ID", "TYPE", "SITE_BIOPSY", "SEX")
+Blueprint_sample_ann$Batch <- 'Blueprint'
+Blueprint_sample_ann$SAMPLE_ID <- gsub('-', '_', Blueprint_sample_ann$SAMPLE_ID)
+Blueprint_sample_ann$TYPE <- gsub('-', '_', Blueprint_sample_ann$TYPE)
+Blueprint_sample_ann$SEX <- gsub('Male', 'M', Blueprint_sample_ann$SEX)
+Blueprint_sample_ann$SEX <- gsub('Female', 'F', Blueprint_sample_ann$SEX)
+Blueprint_sample_ann <- Blueprint_sample_ann %>% filter(SAMPLE_ID %in% colnames(bval))
+
+# Read in the GEO sample annotation
+GEO_sample_ann <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/GEO/GEO_Annotation.tsv",
+                             sep = "\t",
+                             header = TRUE,
+                             na.strings=c("","NA")) %>%
+  dplyr::select(SAMPLE_ID, TYPE)
+GEO_sample_ann$Batch <- 'GSE255869'
+GEO_sample_ann <- GEO_sample_ann %>% filter(SAMPLE_ID %in% colnames(bval))
+
+# Read in clinical data
+clinical_data <- read.csv(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/db/CLN/20240116_clinical_data.csv", 
+                          header = TRUE, na.strings = c("","NA")) %>%
+  dplyr::select(LY_FL_ID, ANN_ARBOR_STAGE, SEX, T_14_18, PRIM_TX_CAT, CODE_PFS, CODE_TRANSF, CODE_OS, PFS, TTT, OS)
+
+# Left outer join sample sheet and sample annotation
+df1 <- merge(x = Sample_Annotations,
+             y = targets,
+             by.x = "SAMPLE_ID",
+             by.y = "SAMPLE_ID",
+             all.x = TRUE)
+
+df2 <- merge(x = df1,
+             y = clinical_data,
+             by.x = "LY_FL_ID",
+             by.y = "LY_FL_ID",
+             all.x = TRUE)
+
+Sample_Annotations <- df2
+
+dim(Sample_Annotations)
+remove(df1, df2)
+
+# Combine sample annotations
+Sample_Annotations <- bind_rows(Sample_Annotations, Benign_LN_sample_ann, NIH_sample_ann, EGA_sample_ann, GEO_sample_ann, Blueprint_sample_ann)
+dim(Sample_Annotations)
+table(Sample_Annotations$TYPE)
+table(Sample_Annotations$Batch)
+
+# Read in methylation clustering information
+epitypes <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/02_Clustering/450k/modified_InfiniumPurify/Enmix_quantile_with_QCinfo_Harman_All/FL_epitypes.tsv",
+                       sep = "\t", header = TRUE, na.strings = c("","NA"))
+
+epitypes <- epitypes %>% filter(SAMPLE_ID %in% colnames(bval))
+
+Sample_Annotations <- merge(x = Sample_Annotations,
+                            y = epitypes,
+                            by.x = "SAMPLE_ID",
+                            by.y = "SAMPLE_ID",
+                            all.x = TRUE)
+
+dim(Sample_Annotations)
+table(Sample_Annotations$TYPE)
+table(Sample_Annotations$epitypes)
+
+# Read in ClusterAIC information
+flexmix_clust <- read.table(file = "/Users/rajesh/Dropbox/Documents/CAN_IMG/01_PMCRC/Projects/00_Projects/01_FLOMICS/00_Methylation_Analysis/00_Minfi/db/ClusterAIC/2023-05-06_14_GMM_Cluster_Labels_flexmix_clusters.txt", 
+                            sep = "\t", header = TRUE, na.strings = c("","NA")) %>% 
+                            dplyr::select(SAMPLE_ID, ClusterAIC)
+
+flexmix_clust <- flexmix_clust %>% filter(SAMPLE_ID %in% colnames(bval))
+
+Sample_Annotations <- merge(x = Sample_Annotations,
+            y = flexmix_clust,
+            by.x = "SAMPLE_ID",
+            by.y = "SAMPLE_ID",
+            all.x = TRUE)
+
+Sample_Annotations <- Sample_Annotations %>%
+  mutate(ClusterAIC = ifelse(is.na(ClusterAIC), "Not Assigned", ClusterAIC))
+
+table(Sample_Annotations$ClusterAIC)
+
+row.names(Sample_Annotations) <- Sample_Annotations$SAMPLE_ID
+
+# subset of merged methylation data
+bval <- bval[, Sample_Annotations$SAMPLE_ID]
+mval <- mval[, Sample_Annotations$SAMPLE_ID]
+dim(bval)
+dim(mval)
+
+gc()
+
+# Remove Blueprint samples before clustering
+Sample_Annotations <- Sample_Annotations %>% filter(TYPE == "FL" | TYPE == "FL_A" | TYPE == "FL_B") %>% data.frame()
+
+# Calculate Samples by Type per Cluster
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'FL_A'] <- 'FL'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'FL_B'] <- 'FL'
+
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'DLBCL_EGA'] <- 'DLBCL'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'DLBCL_EN'] <- 'DLBCL'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'DLBCL_GC'] <- 'DLBCL'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'DLBCL_nonGC'] <- 'DLBCL'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 't_DLBCL'] <- 'DLBCL'
+
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'Benign_LN'] <- 'Normal'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'LN_NN_A'] <- 'Normal'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'LN_NN_B'] <- 'Normal'
+Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'RLN'] <- 'Normal'
+
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'Normal_GCB'] <- 'gcBC'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'bm_PC'] <- 'bm_PC'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'gcBC'] <- 'gcBC'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'memBC'] <- 'memBC'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 'naiBC'] <- 'naiBC'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 't_PC'] <- 't_PC'
+# Sample_Annotations$TYPE[Sample_Annotations$TYPE == 't_naiBC'] <- 't_naiBC'
+
+table(Sample_Annotations$Batch)
+table(Sample_Annotations$TYPE)
+table(Sample_Annotations$epitypes)
+
+# Rename C1, C2, C3, and UC in the epitypes column by appending their corresponding TYPE values
+Sample_Annotations <- Sample_Annotations %>%
+  mutate(epitypes = case_when(
+    epitypes %in% c("C1", "C2", "C3", "UC") ~ paste0(epitypes, "_", TYPE),  # Rename with TYPE
+    TRUE ~ epitypes  # Keep other values unchanged
+  ))
+
+table(Sample_Annotations$Batch)
+table(Sample_Annotations$TYPE)
+table(Sample_Annotations$epitypes)
+
+# subset of merged methylation data
+bval <- bval[, Sample_Annotations$SAMPLE_ID]
+mval <- mval[, Sample_Annotations$SAMPLE_ID]
+dim(bval)
+dim(mval)
+
+# Write TYPE and Batch Information
+df1 <- Sample_Annotations[c("SAMPLE_ID", "TYPE","Batch")]
+write.table(df1, file="TYPE_INFO.txt", sep="\t", quote = FALSE, row.names = FALSE)
+
+gc()
+
+# # Sample data: Assuming Sample_Annotations has 'epitypes' and 'TYPE'
+# # Step 1: Count occurrences of each Sample_Type within each Epitype
+# proportion_data <- Sample_Annotations %>%
+#   group_by(epitypes, TYPE) %>%
+#   summarise(Count = n(), .groups = "drop") %>%
+#   group_by(epitypes) %>% 
+#   mutate(Proportion = Count / sum(Count),  # Normalize to make proportions sum to 1 within each epitype
+#          Proportion_Label = paste(Count, " (", scales::percent(Proportion, accuracy = 0.1), ")", sep = ""))  # Create label with count and percentage
+# 
+# # Convert factors for proper ordering in plot
+# proportion_data$TYPE <- factor(proportion_data$TYPE, levels = c("Normal", "FL", "t_DLBCL", "DLBCL"))
+
+# # Define custom colors with a slightly darker tone
+# sample_colors <- c(
+#   "Normal" = "#6F8CD1",  # Slightly darker blue
+#   "FL" = "#71A15D",      # Slightly darker green
+#   "t_DLBCL" = "#E88C41",  # Slightly darker orange
+#   "DLBCL" = "#C67073"     # Slightly darker red
+# )
+# 
+# pdf("Proportion.pdf", width = 6.5, height = 6)
+# 
+# ggplot(proportion_data, aes(x = epitypes, y = Proportion, fill = TYPE)) +
+#   
+#   # Plot all bars first WITHOUT outline
+#   geom_bar(data = subset(proportion_data, epitypes == "t_DLBCL"), 
+#            stat = "identity", position = "stack", width = 0.7) +
+#   
+#   # Plot bars WITH outline for everything EXCEPT "t_DLBCL"
+#   geom_bar(data = subset(proportion_data, epitypes != "t_DLBCL"), 
+#            stat = "identity", position = "stack", width = 0.7, 
+#            color = "black", linewidth = 0.1) +
+#   
+#   scale_fill_manual(values = sample_colors) +  
+#   labs(x = "Epitype",
+#        y = "Proportion",
+#        fill = "Type") +
+#   theme_minimal() +
+#   theme(
+#     axis.title = element_text(size = 16, face = "bold"),
+#     axis.text = element_text(size = 14),
+#     legend.text = element_text(size = 14),
+#     legend.title = element_text(size = 14, face = "bold"),
+#     axis.line = element_line(),
+#     axis.line.x.top = element_blank(),
+#     axis.line.y.right = element_blank(),
+#     legend.position = "bottom",
+#     legend.key.size = unit(0.8, "cm"),
+#     legend.box.spacing = unit(0.2, "cm"),
+#     plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+#     plot.margin = margin(0.5, 0.8, 0.5, 0.5, "cm")
+#   ) +
+#   
+#   # Add text labels inside bars
+#   geom_text(aes(label = Proportion_Label), 
+#             position = position_stack(vjust = 0.5),  
+#             size = 4, color = "black", fontface = "bold") +  
+#   guides(fill = guide_legend(nrow = 1, byrow = TRUE))  
+# 
+# dev.off()
+
+
+# Step 1: Count occurrences of each ClusterAIC category within each epitype
+cluster_aic_data <- Sample_Annotations %>%
+  filter(epitypes != "UC_FL") %>%  # Remove epitype = "UC"
+  filter(ClusterAIC != "Not Assigned") %>%
+  group_by(epitypes, ClusterAIC) %>%
+  summarise(Count = n(), .groups = "drop") %>%
+  group_by(epitypes) %>% 
+  mutate(Proportion = Count / sum(Count),  # Normalize to make proportions sum to 1 within each epitype
+         Proportion_Label = paste(Count, " (", scales::percent(Proportion, accuracy = 0.1), ")", sep = ""))  # Create label with count and percentage
+
+# Convert factors for proper ordering in plot
+cluster_aic_data$ClusterAIC <- factor(cluster_aic_data$ClusterAIC, levels = c("CS", "TT", "GM", "Q", "AR", "Not Assigned"))
+
+# Define custom colors for ClusterAIC groups
+cluster_aic_colors <- c(
+  "CS" = "#ffa9a3",
+  "TT" = "#e7e8b0",
+  "GM" = "#addece",
+  "Q"  = "#a2cfe0",
+  "AR" = "#e7c0eb",
+  "Not Assigned" = "#c9c9c9"
+)
+
+# Rename epitypes
+table(cluster_aic_data$epitypes)
+
+cluster_aic_data$epitypes[cluster_aic_data$epitypes == 'C1_FL'] <- 'iFL'
+cluster_aic_data$epitypes[cluster_aic_data$epitypes == 'C2_FL'] <- 'aFL'
+cluster_aic_data$epitypes <- factor(cluster_aic_data$epitypes, levels = c("iFL", "aFL")) # reorder
+
+table(cluster_aic_data$epitypes)
+
+# Save plot as PDF
+pdf("ClusterAIC.pdf", width = 6.5, height = 6)
+
+ggplot(cluster_aic_data, aes(x = epitypes, y = Proportion, fill = ClusterAIC)) +
+  
+  # Plot all bars first WITHOUT outline
+  geom_bar(data = subset(cluster_aic_data, epitypes == "t_DLBCL"), 
+           stat = "identity", position = "stack", width = 0.7) +
+  
+  # Plot bars WITH outline for everything EXCEPT "t_DLBCL"
+  geom_bar(data = subset(cluster_aic_data, epitypes != "t_DLBCL"), 
+           stat = "identity", position = "stack", width = 0.7, 
+           color = "black", linewidth = 0.1) +
+  
+  scale_fill_manual(values = cluster_aic_colors, na.value = "#c9c9c9") +  
+  labs(x = "Epitypes",
+       y = "Proportion",
+       fill = "FL Genetic Subtypes") +
+  theme_minimal() +
+  theme(
+    axis.title = element_text(size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text = element_text(size = 14),
+    legend.text = element_text(size = 14),
+    legend.title = element_text(size = 14, face = "bold"),
+    axis.line = element_line(),
+    axis.line.x.top = element_blank(),
+    axis.line.y.right = element_blank(),
+    legend.position = "bottom",
+    legend.key.size = unit(0.8, "cm"),
+    legend.box.spacing = unit(0.2, "cm"),
+    plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+    plot.margin = margin(0.5, 0.8, 0.5, 0.5, "cm")
+  ) +
+  
+  # Add text labels inside bars
+  geom_text(aes(label = Proportion_Label), 
+            position = position_stack(vjust = 0.5),  
+            size = 4, color = "black", fontface = "bold") +  
+  guides(fill = guide_legend(nrow = 1, byrow = TRUE))  
+
+dev.off()
